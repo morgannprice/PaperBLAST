4cd1de62d3d06906a0d8b65ef0c2b81b59da0343
Morgan Price <funwithwords26@gmail.com>
2017-02-21 21:18:32 -0800

    Set up run_snippets.pl

--- bin/elsevierFetch.pl
+++ bin/elsevierFetch.pl
@@ -1,19 +1,14 @@
 #!/usr/bin/perl -w
-# This script is obsolete -- see elsevierFetch.pl instead
 use strict;
 use Getopt::Long;
 use FindBin qw($Bin);
 use lib "$Bin/../lib";
 use Miner;
 
-my $snippetBefore = 20;
-my $snippetAfter = 35;
-my $maxChar = 150;
-
 my $usage = <<END
 elsevierSnippets.pl -keyfile key -papers epmc_pop.papers -journals elsevier_journals2 -dir cache
 	The keyfile should contain a single line with the APIkey.
-	Caches the fetched papers in cache/pubmed_*.xml
+	Places the fetched papers in cache/pubmed_*.xml
 END
     ;
 
@@ -54,37 +49,30 @@ END
     my %fail = (); # pmId => 1 if failed to fetch it
     open(PAPERS, "<", $papersfile) || die "Cannot read $papersfile";
     while(my $line = <PAPERS>) {
-        chomp $line;
-        my ($queryId, $queryTerm, $pmcId, $pmId, $doi, $title, $authors, $journal, $year, $isOpen) = split /\t/, $line;
-        die "Invalid input in $papersfile" unless defined $isOpen;
-        die "Invalid input in $papersfile: isOpen = $isOpen" unless $isOpen eq "1" || $isOpen eq "0";
-        next unless $isOpen eq "0" && $pmId ne "" && exists $journals{$journal};
-        $nTry++;
-        # print STDERR "Working on pubmed:$pmId from $journal\n";
-        my $file = "$cachedir/pubmed_$pmId.xml";
-        if (exists $fail{$pmId}) {
-            $nFail++;
-            next;
-        } elsif (-e $file) {
-            $nCache++;
-        } elsif (! &ElsevierFetch($pmId, $key, $file, $journal)) {
-            $fail{$pmId} = 1;
-            $nFail++;
-            next;
-        } else {
-            $nFetch++;
-        }
+      chomp $line;
+      my ($queryId, $queryTerm, $pmcId, $pmId, $doi, $title, $authors, $journal, $year, $isOpen) = split /\t/, $line;
+      die "Invalid input in $papersfile" unless defined $isOpen;
+      die "Invalid input in $papersfile: isOpen = $isOpen" unless $isOpen eq "1" || $isOpen eq "0";
+      next unless $isOpen eq "0" && $pmId ne "" && exists $journals{$journal};
+      my $file = "$cachedir/pubmed_$pmId.xml";
+      $nTry++;
+      if (exists $fail{$pmId}) {
+        $nFail++;
+      } elsif (-e $file) {
+        $nCache++;
+      } elsif (! &ElsevierFetch($pmId, $key, $file, $journal)) {
+        $fail{$pmId} = 1;
+        $nFail++;
+      } else {
         my $text = &XMLFileToText($file);
         if (!defined $text) {
-            print STDERR "Warning: error reading XML for pubmed id $pmId\n";
-            unlink($file);
-            $nFail++;
+          print STDERR "Warning: error reading XML for pubmed id $pmId\n";
+          unlink($file);
+          $nFail++;
         } else {
-            my @snippets = &TextSnippets($text, $queryTerm, $snippetBefore, $snippetAfter, $maxChar);
-            foreach my $snippet (@snippets) {
-                print join("\t", $pmcId, $pmId, $queryTerm, $queryId, $snippet)."\n";
-            }
+          $nFetch++;
         }
+      }
     }
     print STDERR "Tried $nTry fetched $nFetch cached $nCache failed $nFail\n";
 }
---
+++ bin/run_search.pl
@@ -0,0 +1,55 @@
+#!/usr/bin/perl -w
+# The third phase of the pipeline: running queries against EuropePMC
+use strict;
+use Getopt::Long;
+use FindBin qw($Bin);
+
+my $usage = <<END
+run_search.pl -in downloads -work work [ -test ]
+
+Given queries in work/comb.query (computed by run_terms.pl), run the
+queries against EuropePMC.
+END
+;
+
+my ($indir, $workdir, $test);
+
+sub maybe_run($) {
+    my ($cmd) = @_;
+    if (defined $test) {
+        print STDERR "Would run\t$cmd\n";
+    } else {
+        print STDERR "Running $cmd\n";
+        system($cmd) == 0
+            || die "Error running $cmd: $!\n";
+    }
+}
+
+die $usage
+  unless GetOptions('in=s'=> \$indir,
+                    'work=s' => \$workdir,
+                    'test' => \$test)
+  && @ARGV == 0
+  && defined $indir && defined $workdir;
+
+print STDERR "Test mode\n" if defined $test;
+
+my $infile = "$workdir/comb.query";
+unless (-e $infile) {
+  if (defined $test) {
+    print STDERR "Warning: no input file $infile\n";
+  } else {
+    die "No input file $infile\n";
+  }
+}
+
+&maybe_run("$Bin/queryEuropePMCBatch.pl -in $infile -out $workdir/epmc.part1 -dir $workdir");
+if (defined $test || ! -z "$infile.fail") {
+  &maybe_run("$Bin/join.pl -header 0 -match 1.1=2.3 -ignore 1.1 $workdir/epmc.part1.fail $infile > $infile.fail");
+  &maybe_run("$Bin/queryEuropePMCBatch.pl -in $infile.fail -out $workdir/epmc.part2 -dir $workdir");
+} else {
+  &maybe_run("echo -n > $infile.fail");
+  &maybe_run("echo -n > $workdir/epmc.part2");
+}
+&maybe_run("cat $workdir/epmc.part1 $workdir/epmc.part2 > $workdir/epmc");
+print STDERR "Success\n" if ! defined $test;
