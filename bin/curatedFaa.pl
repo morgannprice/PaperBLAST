b821871295a48994608eecbae22795e985e7c5a9
Morgan Price <funwithwords26@gmail.com>
2018-11-12 16:26:01 -0800

    working

---
+++ bin/curatedFaa.pl
@@ -0,0 +1,101 @@
+#!/usr/bin/perl -w
+use strict;
+
+use DBI;
+use FindBin qw{$Bin};
+use lib "$Bin/../lib";
+use pbutils; # for ReadFastaEntry()
+use Getopt::Long;
+
+my $usage = <<END
+Usage: curatedFaa.pl -db litsearch.db -uniq uniq.faa -out curated.faa [ -filtered filtered ]
+  Filters out curated entries that are probably not actually characterized -- these are
+  written to the filtered file (if it is specified).
+END
+;
+
+my ($dbfile, $uniqfile, $outfile, $filterfile);
+die $usage
+  unless GetOptions('db=s' => \$dbfile,
+                    'uniq=s' => \$uniqfile,
+                    'out=s' => \$outfile,
+                    'filter=s' => \$filterfile)
+  && defined $dbfile && defined $uniqfile && defined $outfile
+  && @ARGV ==0;
+die "No such file: $dbfile\n" unless -e $dbfile;
+die "No such file: $uniqfile\n" unless -e $uniqfile;
+my $dbh = DBI->connect("dbi:SQLite:dbname=$dbfile","","",{ RaiseError => 1 }) || die $DBI::errstr;
+
+my %curatedIds = ();
+my $genes = $dbh->selectall_arrayref("SELECT db,protId,desc FROM CuratedGene");
+my %filtered = (); # db => protId => 1
+foreach my $gene (@$genes) {
+  my ($db, $protId, $desc) = @$gene;
+  # Curated annotations for proteins that are not actually characterized often match these patterns
+  # [DUPF]+ matches PF or UPF or DUF, which together with numbers are common uninformative annotations
+  # descriptions like protein yyyY occur in EcoCyc
+  # These uniformative patterns usually occur at the beginning of the description, but in EcoCyc
+  # they may appear after another comment like "Rac prophage;"
+  # In CharProtDB, "probable" and "putative" are often not informative
+  # In EcoCyc, "putative" is often not informative
+  # In other databases, these terms often indicate that there is genetic evidence but
+  # not biochemical proof of the exact role; decided to keep those.
+  my $maybe = $desc =~ m/^putative/i || $desc =~ m/^probable/i;
+  if ($desc =~ m/^uncharacterized/i
+      || $desc =~ m/\buncharacterized protein/i
+      || $desc =~ m/^(DUF|UPF|PF)\d+ family protein/i
+      || $desc =~ m/^(DUF|UPF|PF)\d+ protein/i
+      || $desc =~ m/; (DUF|UPF|PF)\d+ family protein/i
+      || $desc =~ m/; (DUF|UPF|PF)\d+ protein/i
+      || ($desc =~ m/^protein [a-zA-Z]+$/i && $db ne "SwissProt")
+      || ($desc =~ m/^putative/i && ($db eq "CharProtDB" || $db eq "ecocyc"))
+      || ($desc =~ m/^probable/i && $db eq "CharProtDB")) {
+    $filtered{$db}{$protId} = $desc;
+  } else {
+    $curatedIds{$db . "::" . $protId} = 1;
+  }
+}
+
+my %isDup = ();
+my $seqdups = $dbh->selectall_arrayref(qq{SELECT sequence_id, duplicate_id FROM SeqToDuplicate WHERE duplicate_id LIKE "%::%"});
+my %keepIds = (); # id => list of curated ids that are duplicates of it
+foreach my $row (@$seqdups) {
+  my ($seqId, $dupId) = @$row;
+  if (exists $curatedIds{$dupId}) {
+    push @{ $keepIds{$seqId} }, $dupId;
+    $isDup{$dupId} = 1;
+  }
+}
+
+my %written = ();
+open(my $fhU, "<", $uniqfile) || die "Cannot read $uniqfile";
+open(my $fhOut, ">", $outfile) || die "Cannot write to $outfile";
+my $state = {};
+while (my ($uniqId,$sequence) = ReadFastaEntry($fhU, $state)) {
+  if (exists $curatedIds{$uniqId} || exists $keepIds{$uniqId}) {
+    print $fhOut ">${uniqId}\n$sequence\n";
+    $written{$uniqId} = 1;
+  }
+}
+close($fhU) || die "Error reading $uniqfile";
+close($fhOut) || die "Error writing to $outfile";
+foreach my $curatedId (keys %curatedIds) {
+  die "Did not write a sequence for $curatedId\n"
+    unless exists $written{$curatedId} || exists $isDup{$curatedId};
+}
+print STDERR join(" ", "Found", scalar(keys %curatedIds), "characterized entries and wrote",
+                  scalar(keys %written), "different", "sequences", "to", $outfile)."\n";
+
+if (defined $filterfile) {
+  open (my $fh, ">", $filterfile) || die "Cannot write to $filterfile";
+  foreach my $db (sort keys %filtered) {
+    foreach my $id (sort keys %{ $filtered{$db} }) {
+      my $desc = $filtered{$db}{$id};
+      print $fh "$db\t$id\t$desc\n";
+    }
+  }
+  close($fh) || die "Error writing to $filterfile";
+}
+foreach my $db (sort keys %filtered) {
+  print STDERR "Filtered out from $db: " . scalar(keys %{ $filtered{$db} }) . " entries that are probably uncharacterized.\n";
+}
