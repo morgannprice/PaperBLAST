4dcfca1c75a8b258a4219d0419d84ebf335604d8
Morgan Price <funwithwords26@gmail.com>
2021-02-16 11:32:55 -0800

    gapsearch.pl uses steps.db; also uses all pathways (by default) and output table has better column names

--- bin/gapsearch.pl
+++ bin/gapsearch.pl
@@ -5,108 +5,97 @@
 
 use strict;
 use Getopt::Long;
-use FindBin qw($Bin);
-use lib "$Bin/../lib";
+use DBI;
+use FindBin qw($RealBin);
+use lib "$RealBin/../lib";
 use pbutils qw{ReadTable};
 use Steps qw{ReadOrgTable ReadOrgProtein ParseOrgLocus};
 
 {
   my $nCPU = $ENV{MC_CORES} || 4;
-  my @fields = qw{locusId type curatedId bits locusBegin locusEnd cBegin cEnd cLength identity};
+  my @fields = qw{locusId type queryId bits locusBegin locusEnd qBegin qEnd qLength identity};
   my $fields = join(" ", @fields);
 
   my $usage = <<END
-Usage: gapsearch.pl -orgs prefix -query queryfile1 ... queryfileN -out hitsfile
+Usage: gapsearch.pl -orgs prefix -set set -out hitsfile
 
 The output file is tab-delimited with fields
 $fields
-where type is either hmm or blast, curatedId is the
-query field of the query file or is curated:query,
+where type is either hmm or blast,
+queryId is curatedIds, uniprot:uniprotId, curated2:protId, or hmmId,
 and identity is missing for hits of type hmm.
 
 Optional arguments:
+ -pathway pathwayId  (By default, runs all pathways)
+ -dir $RealBin/../tmp/path.set
  -nCPU $nCPU -- number of CPUs to use (defaults to the MC_CORES
                 environment variable, or 4)
  -verbose -- set for more verbose output to standard error
 END
 ;
 
-  my @queryfiles;
-  my ($hmmdir, $orgprefix, $outfile, $verbose);
+  my ($orgprefix, $set, $outFile, $pathSpec, $inDir, $verbose);
   die $usage
-    unless GetOptions('query=s{1,}' => \@queryfiles,
-                      'orgs=s' => \$orgprefix,
-                      'out=s' => \$outfile,
+    unless GetOptions('orgs=s' => \$orgprefix,
+                      'set=s' => \$set,
+                      'out=s' => \$outFile,
+                      'pathway=s' => \$pathSpec,
+                      'dir=s' => \$inDir,
                       'nCPU=i' => \$nCPU,
                       'verbose' => \$verbose)
       && defined $orgprefix
-      && @queryfiles > 0
-      && defined $outfile;
+      && defined $set
+      && defined $outFile;
   die "Must request at least one CPU\n" unless $nCPU >= 1;
-  foreach my $file (@queryfiles) {
-    die "No such file: $file\n" unless -e $file;
-  }
-  my $bindir = $Bin;
-  my $hmmsearch = "$bindir/hmmsearch";
-  my $usearch = "$bindir/usearch";
+  $inDir = "$RealBin/../tmp/path.$set" unless defined $inDir;
+  die "No such directory: $inDir\n" unless -d $inDir;
+
+  my $binDir = $RealBin;
+  my $hmmsearch = "$binDir/hmmsearch";
+  my $usearch = "$binDir/usearch";
   foreach my $b ($hmmsearch, $usearch) {
     die "No such file or not executable: $b\n" unless -x $b;
   }
   foreach my $suffix (qw{org faa}) {
     die "No such file: $orgprefix.$suffix\n" unless -e "$orgprefix.$suffix";
   }
+
+  my $dbhS = DBI->connect("dbi:SQLite:dbname=${inDir}/steps.db","","",{ RaiseError => 1 }) || die $DBI::errstr;
+
   my @orgs = ReadOrgTable("$orgprefix.org");
   die "The organism table $orgprefix.org has no rows\n" unless @orgs > 0;
   my %orgs = map { $_->{orgId} => $_ } @orgs;
   my $aaIn = "$orgprefix.faa";
 
-  my %hmmFile = (); # hmmId => filename
-  my %curated = (); # curatedId (or uniprot: or curated2:) to sequence
-
-  my @querycol = qw{step type query desc file sequence};
-  foreach my $qfile (@queryfiles) {
-    my $qdir;
-    if ($qfile =~ m!/!) {
-      $qdir = $qfile; $qdir =~ s!/[^/]*$!!;
-    } else {
-      $qdir = ".";
-    }
-    my @queries = ReadTable($qfile, \@querycol);
-    foreach my $query (@queries) {
-      my $id = $query->{query};
-      my $type = $query->{type};
-      $id = "curated2:" . $id if $type eq "curated2";
-      $id = "uniprot:" . $id if $type eq "uniprot";
-
-      if ($type eq "curated" || $type eq "curated2" || $type eq "uniprot") {
-        die "Inconsistent sequence for $id in $qfile\n"
-          if exists $curated{$id} && $curated{$id} ne $query->{sequence};
-        $curated{$id} = $query->{sequence};
-      } elsif ($type eq "hmm") {
-        my $file = $query->{file};
-        die "Invalid characters in hmmfile $file" unless $file =~ m/^[a-zA-Z0-9._-]+$/;
-        $file = "$qdir/$file";
-        die "No such file: $file\n" unless -e $file;
-        die "Inconsistent file for hmm $id in $qfile\n"
-          if exists $hmmFile{$id} && $hmmFile{$id} ne $file;
-        $hmmFile{$id} = $file;
-      } elsif ($type eq "ignore") {
-        ;
-      } else {
-        die "Unrecognized query type $type";
-      }
-    }
+  if (defined $pathSpec) {
+    my $pathObj = $dbhS->selectall_arrayref("SELECT * FROM Pathway WHERE pathwayId = ?",
+                                           {}, $pathSpec);
+    die "Pathway $pathSpec is not in ${inDir}/steps.db\n"
+      unless @$pathObj == 1;
   }
-  print STDERR join(" ", "Comparing", scalar(@orgs), "proteomes to",
-                    scalar(keys %hmmFile), "HMMs",
-                    "and", scalar(keys %curated), "sequences")."\n";
+  my $pathClause = "";
+  $pathClause = qq{ AND pathwayId = "$pathSpec" } if defined $pathSpec;
+  my $querySeq = $dbhS->selectall_arrayref(qq{ SELECT DISTINCT queryType, curatedIds, uniprotId, protId, seq FROM StepQuery
+                                               WHERE queryType IN ("curated", "curated2", "uniprot")
+                                               $pathClause
+                                               ORDER BY queryType, curatedIds, uniprotId, protId },
+                                           { Slice => {} });
+  my $queryHmm = $dbhS->selectall_arrayref(qq{ SELECT DISTINCT hmmId, hmmFileName FROM StepQuery
+                                               WHERE queryType IN ("hmm")
+                                               $pathClause
+                                               ORDER BY hmmId },
+                                          { Slice => {} });
 
-  open (my $fhOut, ">", $outfile) || die "Cannot write to $outfile\n";
+  print STDERR join(" ", "Comparing", scalar(@orgs), "proteomes to",
+                    scalar(@$queryHmm), "HMMs",
+                    "and", scalar(@$querySeq), "sequences")."\n";
+
+  open (my $fhOut, ">", $outFile) || die "Cannot write to $outFile\n";
   print $fhOut join("\t", @fields)."\n";
 
   my %hmmTmp = (); # hmm to tmp hits file
   my $nRunning = 0;
-  foreach my $hmm (sort keys %hmmFile) {
+  foreach my $row (@$queryHmm) {
     $nRunning++;
     if ($nRunning > $nCPU) {
       print STDERR "Waiting\n" if defined $verbose;
@@ -114,14 +103,16 @@ END
       $nRunning--;
       print STDERR "Done waiting\n" if defined $verbose;
     }
-    my $tmpfile = "/tmp/gapsearch.$$.$hmm.domtbl";
-    $hmmTmp{$hmm} = $tmpfile;
+    my $hmmId = $row->{hmmId};
+    my $hmmFileName = $inDir . "/" . $row->{hmmFileName};
+    my $tmpfile = "/tmp/gapsearch.$$.$hmmId.domtbl";
+    $hmmTmp{$hmmId} = $tmpfile;
     if (fork() == 0) {
       # the child process
-      my @cmd = ($hmmsearch, "--cut_tc", "-o", "/dev/null", "--domtblout", $tmpfile, $hmmFile{$hmm}, $aaIn);
-      print STDERR "Running $hmm\n" if defined $verbose;
+      my @cmd = ($hmmsearch, "--cut_tc", "-o", "/dev/null", "--domtblout", $tmpfile, $hmmFileName, $aaIn);
+      print STDERR "Running $hmmId\n" if defined $verbose;
       system(@cmd) == 0 || die "$@cmd failed: $!";
-      print STDERR "$hmm finished\n" if defined $verbose;
+      print STDERR "$hmmId finished\n" if defined $verbose;
       exit(0);
     }
   }
@@ -131,23 +122,36 @@ END
   }
   print STDERR "All " . scalar(keys %hmmTmp) . " HMM analyses complete\n";
 
-  foreach my $hmm (sort keys %hmmFile) {
-    open(my $fhIn, "<", $hmmTmp{$hmm})
-      || die "hmmsearch failed to create $hmmTmp{$hmm}\n";
+  foreach my $row (@$queryHmm) {
+    my $hmmId = $row->{hmmId};
+    open(my $fhIn, "<", $hmmTmp{$hmmId})
+      || die "hmmsearch failed to create $hmmTmp{$hmmId}\n";
     while (my $line = <$fhIn>) {
       chomp $line;
       next if $line =~ m/^#/;
       my @F = split /\s+/, $line;
       my ($hitId, undef, $hitlen, $hmmName, undef, $hmmLen, $seqeval, $seqscore, undef, undef, undef, undef, $domeval, $domscore, undef, $qbeg, $qend, $hitbeg, $hitend) = @F;
-      print $fhOut join("\t", $hitId, "hmm", $hmm, $domscore, $hitbeg, $hitend, $qbeg, $qend, $hmmLen, "")."\n";
+      print $fhOut join("\t", $hitId, "hmm", $hmmId, $domscore, $hitbeg, $hitend, $qbeg, $qend, $hmmLen, "")."\n";
     }
-    unlink($hmmTmp{$hmm});
+    unlink($hmmTmp{$hmmId});
   }
 
   my $cfile = "/tmp/gapsearch.$$.curated";
+  my %queryLen = ();
   open(my $fhC, ">", $cfile) || die "Cannot write to $cfile\n";
-  foreach my $curatedId (sort keys %curated) {
-    print $fhC join("", ">", $curatedId, "\n", $curated{$curatedId}, "\n");
+  foreach my $row (@$querySeq) {
+    my $id;
+    if ($row->{queryType} eq "curated") {
+      $id = $row->{curatedIds};
+    } elsif ($row->{queryType} eq "curated2") {
+      $id = "curated2:" . $row->{protId}
+    } elsif ($row->{queryType} eq "uniprot") {
+      $id = "uniprot:" . $row->{uniprotId};
+    } else {
+      die "Cannot handle query type $row->{queryType}";
+    }
+    $queryLen{$id} = length( $row->{seq} );
+    print $fhC join("", ">", $id, "\n", $row->{seq}, "\n");
   }
   close($fhC) || die "Error writing to $cfile\n";
   my $cmd = join(" ",
@@ -162,15 +166,15 @@ END
   open(my $fhH, "<", "$cfile.hits") || die "usearch did not create $cfile.hits\n";
   while(my $line = <$fhH>) {
     chomp $line;
-    my ($curatedId, $hitId, $identity, $alen, $mm, $gap, $qbeg, $qend, $sbeg, $send, $eval, $bits)
+    my ($id, $hitId, $identity, $alen, $mm, $gap, $qbeg, $qend, $sbeg, $send, $eval, $bits)
       = split /\t/, $line;
-    die "ublast found hit for unknown item $curatedId\n" unless exists $curated{$curatedId};
+    die "ublast found hit for unknown item $id\n" unless exists $queryLen{$id};
     $hitId =~ s/ .*//; # the first part of the header line is the locusId
-    print $fhOut join("\t", $hitId, "blast", $curatedId, $bits, $sbeg, $send, $qbeg, $qend,
-                      length($curated{$curatedId}), $identity)."\n";
+    print $fhOut join("\t", $hitId, "blast", $id, $bits, $sbeg, $send, $qbeg, $qend,
+                      $queryLen{$id}, $identity)."\n";
   }
   close($fhH) || die "Error reading $cfile.hits\n";
   unlink("$cfile");
   unlink("$cfile.hits");
-  close($fhOut) || die "Error writing to $outfile\n";
+  close($fhOut) || die "Error writing to $outFile\n";
 }
