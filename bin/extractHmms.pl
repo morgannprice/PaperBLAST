c98361855ef45f3f7fbe1fd1e8c958b5a61c2916
Morgan Price <funwithwords26@gmail.com>
2021-08-25 09:31:01 -0700

    extractHmms.pl to use correct filenames for PFams

--- bin/extractHmms.pl
+++ bin/extractHmms.pl
@@ -10,11 +10,23 @@ die "Usage: extractHmms.pl steps.db outdirectory\n"
 my $dbh = DBI->connect("dbi:SQLite:dbname=${stepsDb}","","",{ RaiseError => 1 })
   || die $DBI::errstr;
 
+my $hmmFiles = $dbh->selectall_arrayref(qq{SELECT * from StepQuery WHERE queryType = "hmm";},
+                                        { Slice => {} });
+my %hmmIdToFile = ();
+foreach my $row (@$hmmFiles) {
+  $hmmIdToFile{ $row->{hmmId} } = $row->{hmmFileName};
+}
+
 my $hmmIds = $dbh->selectcol_arrayref("SELECT hmmId FROM HMM");
 foreach my $hmmId (@$hmmIds) {
   my ($hmm) = $dbh->selectrow_array("SELECT hmm FROM HMM WHERE hmmId = ?", {}, $hmmId);
   die "No hmm data for $hmmId in $stepsDb" unless $hmm;
-  my $hmmFile = "$outDir/$hmmId.hmm";
+  my $hmmFile = $hmmIdToFile{$hmmId};
+  if (!defined $hmmFile) {
+    print STDERR "Warning: No query row for $hmmId but it is in the HMM table\n";
+    next;
+  }
+  $hmmFile = "$outDir/$hmmFile";
   open(my $fh, ">", $hmmFile) || die "Cannot write to $hmmFile";
   print $fh $hmm;
   close($fh) || die "Error writing to $hmmFile";
