440d3dea599f7a67d647c46306a5c8bf19098e7c
Morgan Price <funwithwords26@gmail.com>
2022-03-14 17:43:19 -0700

    Fix treeSites.js matching to match partial words, not use regular expressions

--- static/treeSites.js
+++ static/treeSites.js
@@ -1,22 +1,29 @@
 
 function leafSearch() {
-  var nMatch = 0;
-  var query = document.getElementById("query").value;
-  if (query == "") return false;
-  var gs = document.getElementsByTagName("g");
-  for (i = 0; i < gs.length; i++) {
-     var list = gs[i].children;
-     if (list.length == 2 && list[0].tagName == "circle" && list[1].tagName=="text") {
-      var textObject = list[1];
-      if (textObject.textContent.match(query)) {
-        textObject.style.display = "inline";
-        textObject.style.fill = "blue";
-        nMatch++;
-      }
+    var nMatch = 0;
+    var query = document.getElementById("query").value;
+    query = query.replace(/^ +/, "");
+    query = query.replace(/ +$/, "");
+    if (query == "") return false;
+    var queryU = query.toUpperCase();
+    var gs = document.getElementsByTagName("g");
+    for (i = 0; i < gs.length; i++) {
+        var list = gs[i].children;
+        if (list.length == 2 && list[0].tagName == "circle" && list[1].tagName=="text") {
+            var textObject = list[1];
+            var content = textObject.textContent.toUpperCase();
+            // match partial words at beginning or after space
+            if (content.substring(0, queryU.length) == queryU
+                || content.indexOf(" " + queryU) >= 0) {
+                textObject.style.display = "inline";
+                textObject.style.fill = "blue";
+                nMatch++;
+            }
+        }
     }
-  }
-  document.getElementById("searchStatement").textContent = nMatch + " " + "matched";
-  return false;
+    document.getElementById("searchStatement").textContent = nMatch + " matched "
+        + query + " (including partial matches to words)";
+    return false;
 }
 
 function leafClear() {
